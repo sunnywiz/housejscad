5f64642e86738eb03a2bb2a8c4c2278f2df7a96d
sunnywiz <sunnywiz@gmail.com>
2015-01-27 21:58:45 -0500

    Now chunking in style

--- house.js
+++ house.js
@@ -5,6 +5,25 @@
 // revision: 0.003
 // tags: Logo,Intersection,Sphere,Cube
 
+function  IsAllTheSame(twoD,x,y,dx,dy,ch) { 
+	for (var y2 = y; y2<y+dy; y2++) { 
+		for (var x2 = x; x2<x+dx; x2++) { 
+		   if (typeof(twoD[y2]) == 'undefined') return false; 
+		   if (twoD[y2][x2] != ch) return false; 
+		}
+	}
+	return true; 
+}
+
+function SetAsConsumed(twoD,x,y,dx,dy) { 
+	for (var y2 = y; y2<y+dy; y2++) { 
+		for (var x2 = x; x2<x+dx; x2++) { 
+		   twoD[y2][x2] = undefined; 
+		}
+	}
+	return true; 
+}
+
 function main() {
     
     var segments=[]; 
@@ -58,6 +77,25 @@ function main() {
 		}
 	}
 
+	// ordered by x times y -- most thingies grabbed. 
+	var tryBuckets = [ [6,6],        //36
+					   [6,5],[5,6],  //30 
+					   [5,5],        //25
+					   [6,4],[4,6],  //24
+					   [4,5],[5,4],  //20
+					   [6,3],[3,6],  //18
+					   [4,4],        //16
+					   [3,5],[5,3],  //15
+					   [4,3],[3,4],[6,2],[2,6],  //12
+					   [2,5],[5,2],  //10
+					   [3,3],        // 9
+					   [4,2],[2,4],  // 8
+					   [3,2],[2,3],[6,1],[1,6], // 6
+					   [1,5],[5,1],  // 5
+					   [2,2],[4,1],[1,4], // 4
+					   [3,1],[1,3],  // 3
+					   [2,1],[1,2] ]; // 2
+	
 	// TODO: idea is to look for "groups" of things, like >, so can replace the whole thing with 
 	// a set of steps.   Only when we get steps, though. 
 	
@@ -66,17 +104,29 @@ function main() {
     	for (x = 0; x < xMax+10; x++) { 
 			var ch = twoD[y][x]; 
 			if (ch in translate) { 
-				var s = translate[ch] (); 
-				s = s.translate([x,y,0]); 
-				segments.push(s);
+			
+				var found = false; 
+				// see how much we can grab
+				for (var tbi = 0; !found && tbi < tryBuckets.length; tbi++) { 
+					var dx = tryBuckets[tbi][0]; 
+					var dy = tryBuckets[tbi][1]; 
+					
+					if (IsAllTheSame(twoD,x,y,dx,dy,ch)) { 
+						var s = translate[ch] (); 
+						s = s.scale([dx,dy,1]).translate([x,y,0]); 
+						segments.push(s); 
+						SetAsConsumed(twoD,x,y,dx,dy);	
+						found = true; 
+					}
+				}
+				if (!found) { 
+					var s = translate[ch] (); 
+					s = s.translate([x,y,0]); 
+					segments.push(s);
+				}
 			}
 		}
 	} 
     return segments;     
-
-	// TODO: change everything to use scale of 1,1,1; then multiply the whole thing by a scale factor 
-	// at the end of the routine. 
-	
-	// 
 	
 }
