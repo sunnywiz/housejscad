f03d889a34acb20fc2dcf7813a5533702b649a65
sunnywiz <sunnywiz@gmail.com>
2015-01-27 21:20:47 -0500

    Now doing a 2D Grid thing First

--- house.js
+++ house.js
@@ -10,34 +10,36 @@ function main() {
     var segments=[]; 
     var template="##===##########===###\n"+
                  "#...................#\n"+
-                 "=...................#\n"+
-                 "=...................#\n"+
-                 "=...................#\n"+
-                 "#...................#\n"+
-                 "#...................#\n"+
-                 "#...................#\n"+
-                 "#...................#\n"+
+                 "=...................=\n"+
+                 "=.......     .......=\n"+
+                 "=.......     .......=\n"+
+                 "=.......     .......=\n"+
+                 "=...................=\n"+
+                 "=...................=\n"+
+                 "=...................=\n"+
                  "#...................#\n"+
                  "##===##!!!!!!##===###\n";
-				 
+   			 
     var translate = new Object(); 
 	translate['#'] = function() { return cube(1).scale([1,1,10]); }; 
 	translate['.'] = function() { return cube(1); }; 
-	translate['!'] = function() 
-	{ 
+	translate['!'] = function() { 
 		return union([
 			cube(1), 
 			cube(1).translate([0,0,9])
 		]); 
 	}; 
-	translate['='] = function() 
-	{ 
+	translate['='] = function()	{ 
 		return union([
 			cube({size: [1,1,5]}), 
 			cube(1).translate([0,0,9])
 		]); 
 	};
-				 
+
+	var twoD = [];
+	twoD[0] = []; 
+    var xMax = 0; 
+    var yMax = 0; 	
     var x=0; 
     var y=0; 
     for(var i=0, len=template.length; i<len; i++) {
@@ -46,12 +48,27 @@ function main() {
         if (ch == '\n' || ch=='\r') { 
             x = 0; 
             y++; 
-		} else if (ch in translate) { 
-			var s = translate[ch] (); 
-			s = s.translate([x,y,0]); 
-			segments.push(s);
+			twoD[y]=[]; 
+		} else { 
+			if (ch in translate) { 
+			   twoD[y][x] = ch; 
+			   if (x>=xMax) { xMax = x+1; }
+			   if (y>=yMax) { yMax = y+1; }
+			}
 		}
-    }
-    return union(segments);     
+	}
+
+	for (y=0; y<yMax+10; y++) { 
+		if (typeof (twoD[y]) == 'undefined') continue; 
+    	for (x = 0; x < xMax+10; x++) { 
+			var ch = twoD[y][x]; 
+			if (ch in translate) { 
+				var s = translate[ch] (); 
+				s = s.translate([x,y,0]); 
+				segments.push(s);
+			}
+		}
+	} 
+    return segments;     
 
 }
